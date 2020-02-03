384a17de65b612369f83228422505dd9a957ac70
Gabriel Quiroz <46507325+gabrielquirozz@users.noreply.github.com>
2020-02-03 01:56:51 -0600

    .

--- app/src/main/java/com/example/laboratorio3/Main2Activity.kt
+++ app/src/main/java/com/example/laboratorio3/Main2Activity.kt
@@ -1,12 +1,25 @@
 package com.example.laboratorio3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+
+
+    fun  Mensaje (v: View) {
+        val mensaje = editText3.getText().toString()
+        val intent = Intent(v.context, MainActivity::class.java)
+        startActivityForResult(intent, 0)
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/laboratorio3/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio3/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.laboratorio3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ScrollView
-import android.widget.TextView
-import androidx.core.view.isGone
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.activity_main2.view.*
+
 
 class MainActivity : AppCompatActivity() {
 
